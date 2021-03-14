e39d10ac685f5d18be1976d1239fcbce3172949b
Nick <przebienda.n@gmail.com>
2021-03-13 20:45:12 -0500

    doing stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -92,32 +92,65 @@ const Emoji = styled.div`
 }
 `
 const Text = styled.div`
-  margin-top: 24px; 
   font-size: 20px;
   text-align: center;
+`
+
+const Spacer = styled.div`
+  width: 100%;
+  height: 24px;
+`
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledInput = styled.input`
+  margin-left: 12px;
+  height: 20px;
+  border: none;
+  border-bottom: 1px solid black;
+  font-size: 20px;
+background-color: #f2efe4;
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid black;
+  }
 `
 
 export default function Home() {
   return (
     <Container>
       <ContentContainer>
+      
       <TitleContainer>
-      <Emoji>👋</Emoji>
-      <TitleText>Pleb Chat</TitleText>
-
+        <Emoji>👋</Emoji>
+        <TitleText>Pleb Chat</TitleText>
       </TitleContainer>
+      <Spacer />
       <Text>
         Hey, you don't have enough followers to get on stage and you don't know what you'd say in front of all those people anyway.
       </Text>
-
+      
+      <Spacer />
       <Text>
         Pleb Chat is here for you.
       </Text>
-
+      
+      <Spacer />
       <Text>
         Join the conversation. Join Pleb Chat.
       </Text>
 
+      <Spacer />
+      <Spacer />
+      <Spacer />
+      <InputContainer>
+        <Text>Clubhouse room URL:</Text> <StyledInput />
+      </InputContainer>
       </ContentContainer>
     </Container>
   )

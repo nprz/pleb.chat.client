de7fb680bce96ba08eda58b56c55cab8aa66425c
Nick <przebienda.n@gmail.com>
2021-03-19 22:31:45 -0400

    adding callback

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,13 +2,47 @@ import React, { useState, useEffect, useContext } from "react";
 import createAuth0Client from "auth0/auth0-spa-js";
 
 export const Auth0Context = React.createContext();
-export const useAuth0 = () => useContext(Auth0Context)
+export const useAuth0 = () => useContext(Auth0Context);
 
-const DEFAULT_REDIRECT_CALLBACK = () => window.history.replaceState({}, document.title, window.location.pathname);
+const DEFAULT_REDIRECT_CALLBACK = () =>
+  window.history.replaceState({}, document.title, window.location.pathname);
 
 export const Auth0Provider = ({
   children,
-  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK
+  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+  ...initOptions
 }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState();
+  const [user, setUser] = useState();
+  const [auth0Client, setAuth0Client] = useState();
+  const [loading, setLoading] = useState(false);
+  const [popupOpen, setPopupOpen] = useState(false);
 
-}
+  useEffect(() => {
+    const initAuth0 = async () => {
+      const auth0FromHook = await createAuth0Client(initOptions);
+      setAuth0(auth0Client);
+
+      if (
+        window.location.search.includes("code=") &&
+        window.location.search.includes("state=")
+      ) {
+        const { appState } = await auth0FromHook.handleRedirectCallback();
+        onRedirectCallback(appState);
+      }
+
+      const isAuthenticated = await auth0FromHook.isAuthenticated();
+
+      setIsAuthenticated(isAuthenticated);
+
+      if (isAuthenticated) {
+        const user = await auth0FromHook.getUser();
+        setUser(user);
+      }
+
+      setLoading(false);
+    };
+
+    initAuth0();
+  }, []);
+};

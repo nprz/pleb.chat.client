e9749ba253434d857ec5e4b67bd3bdd6471b28d6
Nick <przebienda.n@gmail.com>
2021-05-17 23:21:03 -0400

    comment 8

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ history.listen(({ location }) => {
   ReactGA.pageview(location.pathname);
 });
 
-// comment 7
+// comment 8
 function App() {
   useEffect(() => {
     ReactGA.initialize(trackingId);
